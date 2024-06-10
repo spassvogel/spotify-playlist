e108d2cf485ab0e9e3468aead0742fc5f11821a1
Wouter van den Heuvel <exo_00@hotmail.com>
2024-06-09 21:40:06 -0600

    set base url on GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/spotify-list/' : '/', // for deployment on GH pages
   plugins: [react()],
 })
